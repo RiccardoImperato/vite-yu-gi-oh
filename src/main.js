dfde301fea3fb47f5b5bdb7e947926fee2230b24
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-12 17:44:40 +0100

    font montserrat

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import '../src/assets/scss/style.scss'
+// File Vue di partenza
 import App from './App.vue'
+// File stile di partenza
+import '../src/assets/scss/style.scss'
+// Font Montserrat
+import "@fontsource/montserrat";
 
 createApp(App).mount('#app')
